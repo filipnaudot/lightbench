013f7f240277b5e9b3c0612a64e71cf77ac6c0a6
Filip Naudot <filip.naudot@gmail.com>
2024-10-03 18:14:01 +0200

    chat seems to be working with history

--- chat.py
+++ chat.py
@@ -1,52 +1,53 @@
 import os
 from dotenv import load_dotenv
-
-import torch
-# Hugging face
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-
 load_dotenv()
 
+import torch
+from transformers import pipeline, AutoTokenizer
 
 
-def generate_response(input_text, model, tokenizer, device, max_length=100):
-    # Tokenize the input text
-    inputs = tokenizer(input_text, return_tensors="pt").to(device)
-    
-    # Generate a response using the model
-    with torch.no_grad():
-        outputs = model.generate(
-            inputs["input_ids"], 
-            max_length=max_length, 
-            num_return_sequences=1,
-            do_sample=True,   # Use sampling to create a more conversational response
-            temperature=0.7,  # Adjust temperature for more randomness
-            top_p=0.9,        # Nucleus sampling
-        )
-    
-    # Decode the generated tokens into text
-    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
+
+hf_token = os.getenv("HUGGINGFACE_TOKEN")
+model_id = os.getenv("MODEL_NAME")
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+
+generator = pipeline(
+    "text-generation",
+    model=model_id,
+    torch_dtype=torch.bfloat16,
+    device_map="auto",
+    pad_token_id=tokenizer.eos_token_id,
+)
+
+conversation_history = [
+    {"role": "system", "content": "You are a helpful assistant, that responds as a pirate."}
+]
 
 def main():
-    hf_token = os.getenv("HUGGINGFACE_TOKEN")
-    model_name = os.getenv("MODEL_NAME")
-    tokenizer = AutoTokenizer.from_pretrained(model_name, token=hf_token)
-    model = AutoModelForCausalLM.from_pretrained(model_name, token=hf_token)
-    model.eval()
-
-    # Move the model to GPU
-    device = torch.device("cuda")
-    model.to(device)
-    
     while True:
-        user_input = input("User: ")
+        user_input = input("Enter: ")
+        
         if user_input.lower() in ["exit", "quit"]:
             break
-        bot_response = generate_response(user_input, model, tokenizer, device)
-        print(f"Bot: {bot_response}")
-
+        
+        conversation_history.append({"role": "user", "content": user_input})
+        
+        generation = generator(
+            conversation_history,
+            do_sample=False,
+            temperature=1.0,
+            top_p=1,
+            max_new_tokens=256
+        )
+        
+        response = generation[0]['generated_text'][-1]['content']
+        
+        print(f"\nBot: {response}\n\n")
+        
+        conversation_history.append({"role": "assistant", "content": response})
 
 if __name__ == "__main__":
     main()
