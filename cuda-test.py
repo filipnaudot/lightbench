9700e6120e4bf2b31b9110ee81ee543e4014d745
Filip Naudot <filip.naudot@gmail.com>
2024-10-14 11:08:34 +0200

    Added cuda toolkit check

--- cuda-test.py
+++ cuda-test.py
@@ -1,18 +1,24 @@
+import subprocess
 import torch
 
 def test_cuda():
     if not torch.cuda.is_available():
-        print("CUDA is not available. Please check your driver and CUDA installation.")
+        print("WARNING: CUDA is not available. Please check your driver and CUDA installation.")
         return False
 
     print(f"\n(CUDA available) GPU Name: {torch.cuda.get_device_name(0)}\n")
 
-    # Create two random tensors and move them to GPU
+    try:
+        output = subprocess.check_output(['nvcc', '--version'])
+        print("CUDA Toolkit is installed.\n")
+    except FileNotFoundError:
+        print("WARNING: CUDA Toolkit is not installed.")
+        return False
+
     tensor_size = 20
     tensor_a = torch.rand(tensor_size, tensor_size).cuda()
     tensor_b = torch.rand(tensor_size, tensor_size).cuda()
 
-    # Set up CUDA event objects to record time
     start_event = torch.cuda.Event(enable_timing=True)
     end_event = torch.cuda.Event(enable_timing=True)
     start_event.record()
@@ -30,7 +36,7 @@ def test_cuda():
     # print(tensor_b)
     # print("\nResult of Matrix Multiplication A * B on GPU:")
     # print(result)
-    print(f"\nTime taken for matrix multiplication: {elapsed_time_ms:.3f} ms")
+    print(f"Time taken for matrix multiplication: {elapsed_time_ms:.3f} ms")
 
     return True
 
