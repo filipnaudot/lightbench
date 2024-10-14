050b35dd4db838d51bde1b3a6a24e89f40212dcd
Filip Naudot <filip.naudot@gmail.com>
2024-10-14 13:35:01 +0200

    added print functions for color prints

--- utils.py
+++ utils.py
@@ -1,5 +1,17 @@
 class Colors:
-    WHITE_TEXT = '\033[97m'
+    # BACKGROUND
     BACKGROUND_LIGHT_BLUE = '\033[104m'
-    BACKGROUND_GRAY = '\033[100m'
-    RESET = '\033[0m'
+    BACKGROUND_GRAY       = '\033[100m'
+    # TEXT
+    WHITE_TEXT            = '\033[97m'
+    GREEN_TEXT            = '\033[92m'
+    RED_TEXT              = '\033[91m'
+
+    RESET                 = '\033[0m'
+
+
+def print_green(input, end="\n"):
+    print(f"{Colors.GREEN_TEXT}{input}{Colors.RESET}", end=end)
+
+def print_red(input, end="\n"):
+    print(f"{Colors.RED_TEXT}{input}{Colors.RESET}", end=end)
