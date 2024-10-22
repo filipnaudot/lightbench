0073cefe0c0177fee08183457e04fccd7111f897
Filip Naudot <filip.naudot@gmail.com>
2024-10-22 08:22:50 +0200

    Refactor prompt creation to create_prompts()

--- evaluate.py
+++ evaluate.py
@@ -11,7 +11,22 @@ from code_evaluator import CodeEvaluator
 
 
 
-
+def create_prompts(json_list, system_command):
+    prompts = []
+    for json_str in json_list:
+        result = json.loads(json_str)
+        promt = (
+            [
+                system_command,
+                {
+                    "role": "user",
+                    "content": result["text"] + f' The function should pass the following test: {result["test_list"][0]}.',
+                }
+            ], result["test_list"][1]
+        )
+        prompts.append(promt)
+    
+    return prompts
 
 
 def main(stream: bool = False, QUANTIZE: bool = False):
@@ -32,19 +47,8 @@ def main(stream: bool = False, QUANTIZE: bool = False):
         "role": "system",
         "content": "You are a Python programming assistant. Your task is to write Python functions according to the user's prompt. Respond only with the necessary Python code, including python package imports if needed. Do not provide example usage, only the python function.",
     }
-    prompts = []
-    for json_str in json_list:
-        result = json.loads(json_str)
-        promt = (
-            [
-                system_command,
-                {
-                    "role": "user",
-                    "content": result["text"] + f' The function should pass the following test: {result["test_list"][0]}.',
-                }
-            ], result["test_list"][1]
-        )
-        prompts.append(promt)
+    
+    prompts = create_prompts(json_list, system_command)
     
     models = ["meta-llama/Llama-3.2-3B-Instruct", "meta-llama/Llama-3.1-8B-Instruct"]
     for model in models:
