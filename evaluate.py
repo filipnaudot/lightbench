f32d25061235a062307e134962dad40f4db36542
Filip Naudot <filip.naudot@gmail.com>
2024-10-23 14:36:15 +0200

    more models

--- evaluate.py
+++ evaluate.py
@@ -52,11 +52,17 @@ def main():
     prompts = create_prompts(json_list, system_command)
     
     #                 MODEL NAME                   QUANT  FEW-SHOT
-    models = [("meta-llama/Llama-3.2-3B-Instruct", False, False),
+    models = [("meta-llama/Llama-3.2-1B-Instruct", False, False),
+              ("meta-llama/Llama-3.2-1B-Instruct", False, True),
+              ("meta-llama/Llama-3.2-1B-Instruct", True,  False),
+              ("meta-llama/Llama-3.2-1B-Instruct", True,  True),
+              
+              ("meta-llama/Llama-3.2-3B-Instruct", False, False),
               ("meta-llama/Llama-3.2-3B-Instruct", False, True),
               ("meta-llama/Llama-3.2-3B-Instruct", True,  False),
               ("meta-llama/Llama-3.2-3B-Instruct", True,  True),
-
+              
+              # TODO: Check if model can run on GPU instead of manual remove
               # ("meta-llama/Llama-3.1-8B-Instruct", False, False),
               # ("meta-llama/Llama-3.1-8B-Instruct", False, True),
               ("meta-llama/Llama-3.1-8B-Instruct", True,  False),
