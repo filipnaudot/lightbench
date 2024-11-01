c08600e04d484f1b28ad11beb6440622cc1d3c69
Filip Naudot <filip.naudot@gmail.com>
2024-11-01 15:09:50 +0100

    Refactor func names to be more pythonic

--- code_evaluator.py
+++ code_evaluator.py
@@ -34,7 +34,7 @@ class CodeEvaluator:
         self.streamer:TextIteratorStreamer = TextIteratorStreamer(self.tokenizer, skip_prompt=True)
 
         if self.quantize:
-            self.model = self.load_quantized_model()
+            self.model = self._load_quantized_model()
         else:
             self.model = model_name
 
@@ -49,7 +49,7 @@ class CodeEvaluator:
         )
 
 
-    def load_quantized_model(self):
+    def _load_quantized_model(self):
         print("Loading quantized model...")
         bnb_config = BitsAndBytesConfig(load_in_4bit=True)
 
@@ -61,6 +61,149 @@ class CodeEvaluator:
         )
 
         return model
+
+
+    def _clear_last_row(self):
+        print(f"\r{' ' * 40}\r", end='', flush=True) # Clear last line
+        
+    def _print_test_time(self, index, inference_time, ttft):
+        self._clear_last_row()
+        print(f"\r{index+1} ({inference_time:.2f}s TTFT: {ttft:.2f}s) ", end='')
+
+    def _print_test_status(self):
+        self._clear_last_row()
+        percentage = 0
+        if self.num_test > 0: percentage = (self.passed_test / self.num_test) * 100
+        Printer.print_cyan(f"\rTests Passed: {self.passed_test}/{self.num_test} ({percentage:.2f}%)", end='')
+
+
+    def _handle_stream_output(self, streamer, start_time, ttft_list, print_stream=False):
+        first_token = True
+        for token in streamer:
+            if first_token:
+                ttft = time.time() - start_time
+                ttft_list.append(ttft)
+                first_token = False
+            
+            if print_stream:
+                print(f"{token}", end='', flush=True)
+
+
+    def _preprocess_data(self, data):
+        if f"```python" in data:
+            data = data[data.find(f"```python") + len(f"```python"):]
+            data = data[:data.find("```")]
+        return data
+
+
+    def _timeout_handler(self, signum, frame):
+        raise TimeoutError("Execution timed out!")
+
+
+    def _validate_code(self, code, test, shots):
+        indent_format = f"\033[{30}G" if shots > 0 else f"\033[{25}G"
+        end = "" if self.verbose else "\n"
+
+        full_code_to_execute = f"{code}\n\n{test}"
+        Printer.print_yellow(f"{indent_format}{shots} ", end='')
+        signal.signal(signal.SIGALRM, self._timeout_handler)
+        signal.alarm(10)
+        try:
+            # Redirect stdout for cleaner terminal since executed code might have prints
+            with io.StringIO() as buffer, redirect_stdout(buffer):
+                exec(full_code_to_execute, globals())
+        except AssertionError:
+            Printer.print_red(f"FAILED", end=end)
+            if self.verbose: print(f": test {str(test)} FAILED")
+            return 0, f"There is a logical error in the code. TEST: {str(test)} FAILED"
+        except Exception as error:
+            Printer.print_red(f"FAILED", end=end)
+            if self.verbose: print(f": An error occurred: {error}")
+            return 0, f"ERROR: {error}"
+        except TimeoutError as error:
+            Printer.print_red(f"FAILED", end=end)
+            if self.verbose: print(f": {error}")
+            return 0, f"FAILED: {error}"
+        finally:
+            signal.alarm(0)
+        
+        Printer.print_green(f"PASSED")
+        return 1, "PASSED"
+    
+
+    def _generate_response(self, prompt):
+        ttft_list = []
+
+        start_time = time.time()
+
+        streaming_thread = threading.Thread(target=self._handle_stream_output, args=(self.streamer, start_time, ttft_list))
+        streaming_thread.start()
+
+        generation = self.generator(
+            prompt,
+            streamer=self.streamer,
+            do_sample=False,
+            temperature=1.0,
+            top_p=1,
+            max_new_tokens=512,
+        )
+        end_time = time.time()
+
+        streaming_thread.join()
+        
+        response = generation[0]['generated_text'][-1]['content']
+
+        return response, (end_time-start_time), ttft_list[-1]
+    
+    
+    def _create_few_shot_prompt(self, prompt, response, message):
+        return [
+            *prompt,
+            {
+                "role": "assistant",
+                "content": response
+            },
+            {
+                "role": "user",
+                "content": f' While running that code I received the following: {message}. Can you update the code and fix the problem?',
+            }
+        ]
+    
+
+    def run(self, prompts):
+
+        for index, (prompt, test) in enumerate(prompts):
+            response, inference_time, ttft = self._generate_response(prompt)
+
+            self._print_test_time(index, inference_time, ttft)
+
+            extracted_code = self._preprocess_data(response).strip()
+            passed, message = self._validate_code(extracted_code, test, 0)
+
+            if self.few_shot and not passed:
+                prompt = self._create_few_shot_prompt(prompt, response, message)
+                shots = 1
+
+                while not passed and shots <= 2:
+                    self._print_test_status()
+
+                    response, inference_time, ttft = self._generate_response(prompt)
+                    self._print_test_time(index, inference_time, ttft)
+
+                    extracted_code = self._preprocess_data(response).strip()
+                    passed, message = self._validate_code(extracted_code, test, shots)
+
+                    prompt = self._create_few_shot_prompt(prompt, response, message)
+                    shots += 1
+
+            self.inference_time_list.append(inference_time)
+            self.ttft_list.append(ttft)
+            self.num_test += 1
+            if passed:
+                self.passed_test += 1
+
+            self._print_test_status()
+        print()
     
 
     def cleanup(self):        
@@ -102,147 +245,5 @@ class CodeEvaluator:
         print(json.dumps(summary, indent=4))
 
         os.makedirs("./results", exist_ok=True)
-        with open(f"./results/{self.model_name.replace('/','-')}---quantize={str(self.quantize)}--few_shot={str(self.few_shot)}.json", "w") as file:
+        with open(f"./results/TEST-{self.model_name.replace('/','-')}---quantize={str(self.quantize)}--few_shot={str(self.few_shot)}.json", "w") as file:
             file.write(json.dumps(summary, indent=4))
-
-
-    def clear_last_row(self):
-        print(f"\r{' ' * 40}\r", end='', flush=True) # Clear last line
-        
-    def print_test_time(self, index, inference_time, ttft):
-        self.clear_last_row()
-        print(f"\r{index+1} ({inference_time:.2f}s TTFT: {ttft:.2f}s) ", end='')
-
-    def print_test_status(self):
-        self.clear_last_row()
-        percentage = 0
-        if self.num_test > 0: percentage = (self.passed_test / self.num_test) * 100
-        Printer.print_cyan(f"\rTests Passed: {self.passed_test}/{self.num_test} ({percentage:.2f}%)", end='')
-
-
-    def handle_stream_output(self, streamer, start_time, ttft_list, print_stream=False):
-        first_token = True
-        for token in streamer:
-            if first_token:
-                ttft = time.time() - start_time
-                ttft_list.append(ttft)
-                first_token = False
-            
-            if print_stream:
-                print(f"{token}", end='', flush=True)
-
-
-    def preprocess_data(self, data):
-        if f"```python" in data:
-            data = data[data.find(f"```python") + len(f"```python"):]
-            data = data[:data.find("```")]
-        return data
-
-
-    def timeout_handler(self, signum, frame):
-        raise TimeoutError("Execution timed out!")
-
-
-    def validate_code(self, code, test, shots):
-        indent_format = f"\033[{30}G" if shots > 0 else f"\033[{25}G"
-        end = "" if self.verbose else "\n"
-
-        full_code_to_execute = f"{code}\n\n{test}"
-        Printer.print_yellow(f"{indent_format}{shots} ", end='')
-        signal.signal(signal.SIGALRM, self.timeout_handler)
-        signal.alarm(10)
-        try:
-            # Redirect stdout for cleaner terminal since executed code might have prints
-            with io.StringIO() as buffer, redirect_stdout(buffer):
-                exec(full_code_to_execute, globals())
-        except AssertionError:
-            Printer.print_red(f"FAILED", end=end)
-            if self.verbose: print(f": test {str(test)} FAILED")
-            return 0, f"There is a logical error in the code. TEST: {str(test)} FAILED"
-        except Exception as error:
-            Printer.print_red(f"FAILED", end=end)
-            if self.verbose: print(f": An error occurred: {error}")
-            return 0, f"ERROR: {error}"
-        except TimeoutError as error:
-            Printer.print_red(f"FAILED", end=end)
-            if self.verbose: print(f": {error}")
-            return 0, f"FAILED: {error}"
-        finally:
-            signal.alarm(0)
-        
-        Printer.print_green(f"PASSED")
-        return 1, "PASSED"
-
-
-    def run(self, prompts):
-
-        for index, (prompt, test) in enumerate(prompts):
-            response, inference_time, ttft = self.generate_response(prompt)
-
-            self.print_test_time(index, inference_time, ttft)
-
-            extracted_code = self.preprocess_data(response).strip()
-            passed, message = self.validate_code(extracted_code, test, 0)
-
-            if self.few_shot and not passed:
-                prompt = self.create_few_shot_prompt(prompt, response, message)
-                shots = 1
-
-                while not passed and shots <= 2:
-                    self.print_test_status()
-
-                    response, inference_time, ttft = self.generate_response(prompt)
-                    self.print_test_time(index, inference_time, ttft)
-
-                    extracted_code = self.preprocess_data(response).strip()
-                    passed, message = self.validate_code(extracted_code, test, shots)
-
-                    prompt = self.create_few_shot_prompt(prompt, response, message)
-                    shots += 1
-
-            self.inference_time_list.append(inference_time)
-            self.ttft_list.append(ttft)
-            self.num_test += 1
-            if passed:
-                self.passed_test += 1
-
-            self.print_test_status()
-        print()
-
-    def create_few_shot_prompt(self, prompt, response, message):
-        return [
-            *prompt,
-            {
-                "role": "assistant",
-                "content": response
-            },
-            {
-                "role": "user",
-                "content": f' While running that code I received the following: {message}. Can you update the code and fix the problem?',
-            }
-        ]
-
-
-    def generate_response(self, prompt):
-        ttft_list = []
-
-        start_time = time.time()
-
-        streaming_thread = threading.Thread(target=self.handle_stream_output, args=(self.streamer, start_time, ttft_list))
-        streaming_thread.start()
-
-        generation = self.generator(
-            prompt,
-            streamer=self.streamer,
-            do_sample=False,
-            temperature=1.0,
-            top_p=1,
-            max_new_tokens=512,
-        )
-        end_time = time.time()
-
-        streaming_thread.join()
-        
-        response = generation[0]['generated_text'][-1]['content']
-
-        return response, (end_time-start_time), ttft_list[-1]
