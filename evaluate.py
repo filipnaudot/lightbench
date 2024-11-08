4dee099c32278fa0c7cc9895f86fb2ec4c30e050
Filip Naudot <filip.naudot@gmail.com>
2024-11-08 21:32:48 +0000

    Uncommented all models in model list

--- evaluate.py
+++ evaluate.py
@@ -10,19 +10,19 @@ from text_evaluator import TextEvaluator
 
 
 #                 MODEL NAME                   QUANT  FEW-SHOT
-MODELS = [# ("meta-llama/Llama-3.2-1B-Instruct", False, False),
-          # ("meta-llama/Llama-3.2-1B-Instruct", False, True),
-          # ("meta-llama/Llama-3.2-1B-Instruct", True,  False),
-          # ("meta-llama/Llama-3.2-1B-Instruct", True,  True),
-          # ("meta-llama/Llama-3.2-3B-Instruct", False, False),
-          # ("meta-llama/Llama-3.2-3B-Instruct", False, True),
+MODELS = [("meta-llama/Llama-3.2-1B-Instruct", False, False),
+          ("meta-llama/Llama-3.2-1B-Instruct", False, True),
+          ("meta-llama/Llama-3.2-1B-Instruct", True,  False),
+          ("meta-llama/Llama-3.2-1B-Instruct", True,  True),
+          ("meta-llama/Llama-3.2-3B-Instruct", False, False),
+          ("meta-llama/Llama-3.2-3B-Instruct", False, True),
           ("meta-llama/Llama-3.2-3B-Instruct", True,  False),
           ("meta-llama/Llama-3.2-3B-Instruct", True,  True),
           # TODO: Check if model can run on GPU instead of manual remove
-          #("meta-llama/Llama-3.1-8B-Instruct", False, False),
-          #("meta-llama/Llama-3.1-8B-Instruct", False, True),
-          #("meta-llama/Llama-3.1-8B-Instruct", True,  False),
-          #("meta-llama/Llama-3.1-8B-Instruct", True,  True),
+          ("meta-llama/Llama-3.1-8B-Instruct", False, False),
+          ("meta-llama/Llama-3.1-8B-Instruct", False, True),
+          ("meta-llama/Llama-3.1-8B-Instruct", True,  False),
+          ("meta-llama/Llama-3.1-8B-Instruct", True,  True),
           ]
 
 
