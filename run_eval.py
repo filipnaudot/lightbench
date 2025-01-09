040a8419e8ce60f72165f031daf9793e1dc6b982
Filip Naudot <filip.naudot@gmail.com>
2025-01-09 10:29:04 +0100

    Hotfix: Added evaluate_bias and removed --all option.

--- run_eval.py
+++ run_eval.py
@@ -5,6 +5,8 @@ import argparse
 from configurators.model_setup_configurator import ModelSetupConfigurator 
 from evaluators.code_evaluator import CodeEvaluator
 from evaluators.text_evaluator import TextEvaluator
+from evaluators.cv_bias_evaluator import CVBiasEvaluator
+
 from metrics.llm_judge import LLMJudge
 
 
@@ -46,35 +48,52 @@ def evaluate_text():
 
 
 ####################################################################
+#                               Bias                               #
+####################################################################
+def evaluate_bias():
+    model_setup_conf = ModelSetupConfigurator()
+    models = model_setup_conf.generate_list(use_quantization=True, use_few_shot=False)
+    for model, quantize, _ in models:
+        print(f"\n---------- {model} ----------\n    quantize: {str(quantize)}\n")
+        bias_evaluator = CVBiasEvaluator(model,
+                                         quantize=quantize,
+                                         dataset_path="./data/CVerse/CVerse.json",
+                                         verbose=True)
+        bias_evaluator.run()
+        bias_evaluator.print_summary()
+        bias_evaluator.cleanup()
+
+
+
+####################################################################
 #                                Main                              #
 ####################################################################
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="Evaluation script with options to run code, text, or all evaluations.",
-        usage="python %(prog)s [--code] [--text] [--all]",
-        epilog="Examples:\n  python evaluate.py --code\n  python evaluate.py --all\n  python evaluate.py --text",
+        description="Evaluation script with options to run different evaluations.",
+        usage="python %(prog)s [--code] [--text] [--bias]",
+        epilog="Examples:\n  python evaluate.py --code\n  python evaluate.py --text\n",
         formatter_class=argparse.RawTextHelpFormatter
     )
     
     parser.add_argument('--code', action='store_true', help="Run code evaluation")
     parser.add_argument('--text', action='store_true', help="Run text evaluation")
-    parser.add_argument('--all', action='store_true', help="Run all evaluations")
+    parser.add_argument('--bias', action='store_true', help="Run text evaluation")
 
     return parser.parse_args()
 
 
 def main():
-
     args = parse_args()
+    
     if args.code:
         evaluate_code()
-    elif args.all:
-        evaluate_code()
-        evaluate_text()
     elif args.text:
         evaluate_text()
+    elif args.bias:
+        evaluate_bias()
     else:
-        print("Please provide an argument. Use --code, --all, or --text. Use --help for more information.")
+        print("Please provide an argument. Use --help for more information.")
 
 
 if __name__ == "__main__":
